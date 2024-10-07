3c876b56839a8d76eb8c974270ac873be87f153e
Marko Cvijanović <markocvijanovicmlg@gmail.com>
2024-10-07 17:16:04 +0200

    [INIT] Project Initialisation

---
+++ Chords/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using Microsoft.Win32;
+
+namespace SoundPlayerApp
+{
+    public partial class MainWindow : Window
+    {
+        private MediaPlayer _mediaPlayer;
+        private string _selectedFilePath;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _mediaPlayer = new MediaPlayer();
+        }
+
+        private void UploadChord_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Audio Files (*.wav;*.mp3)|*.wav;*.mp3";
+            openFileDialog.Title = "Select a Sound File";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                _selectedFilePath = openFileDialog.FileName;
+                _mediaPlayer.Open(new Uri(_selectedFilePath));
+                PlayChord.IsEnabled = true;
+            }
+        }
+        private void PlayChord_Click(object sender, RoutedEventArgs e)
+        {
+            if (_selectedFilePath != null)
+            {
+                _mediaPlayer.Position = TimeSpan.Zero;
+                _mediaPlayer.Play();
+            }
+            else
+            {
+                MessageBox.Show("No chord selected!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
